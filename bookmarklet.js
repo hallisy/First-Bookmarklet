0165fa705bcf36bc46df023aafbfb8cef70d9fb2
Nick <nhallisy@gmail.com>
2018-09-28 16:22:23 -0700

    added function

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,4 +1,8 @@
+javascript:(function(){
+	console.log("bookmarklet starting");
+	// do stuff
+	console.log("bookmarklet finished");
+})();
 
-
-const shoerl = https://www.vans.com/shop/solid-colors-slip-on-true-white#hero=1;
+const shoerl = 'https://www.vans.com/shop/solid-colors-slip-on-true-white#hero=1';
 // https://github.com/hallisy/Supreme-Bot/blob/master/content_scripts/cart.js
