80c58203911ccf9e6630d257ba153f377dcb3d39
Nick <nhallisy@gmail.com>
2018-10-15 16:54:16 -0700

    successful url redirect

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,8 +1,12 @@
 javascript:(function(){
-	console.log("bookmarklet starting");
-	// do stuff
+	// redirect to shoe url
+	var loc = window.location;
+	var url = 'https://www.vans.com/shop/solid-colors-slip-on-true-white#hero=0';
+	if (loc != url){
+		alert('Redirecting...');
+		window.location.href = url;
+	}
 	console.log("bookmarklet finished");
 })();
 
-const shoerl = 'https://www.vans.com/shop/solid-colors-slip-on-true-white#hero=1';
 // https://github.com/hallisy/Supreme-Bot/blob/master/content_scripts/cart.js
