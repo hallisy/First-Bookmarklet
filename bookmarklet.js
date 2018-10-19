14b59fade878d1bb35f617347425523916710919
Nick <nhallisy@gmail.com>
2018-10-18 21:31:17 -0700

    changed size,comments to log

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,24 +1,29 @@
 javascript:(function(){
-	// redirect to shoe url
+	console.log("redirect to shoe url");
 	var loc = window.location;
-	var url = 'https://www.vans.com/shop/solid-colors-slip-on-true-white#hero=0';
+	var url = 'https://www.vans.com/shop/slip-on-birch-true-white#hero=0';
 	if (loc != url){
 		alert('Redirecting... select OK to proceed');
 		window.location.href = url;
 	}
-	// select size
-	$('#attr-size').val("4.0 Boys = 5.5 Women").trigger('change');
-	// select quantity
+	
+	console.log("select size");
+	$('#attr-size').val("4.5 Boys = 6.0 Women").trigger('change');
+	
+	console.log("select quantity");
 	$('#quantity-item').val("1").trigger('change');
-	// add to cart
+	
+	console.log("add to cart");
 	var addToCart = document.querySelector('a.product-content-form-add-to-bag.button.primary.add-to-bag-js');
 	addToCart.click();
 
-	// checkout
+	console.log("checkout");
+	
 	var cart = document.querySelector('a.mini-cart-action.button.primary');
 	cart.click();
 	console.log("bookmarklet finished");
 })();
 
 // https://github.com/hallisy/Supreme-Bot/blob/master/content_scripts/cart.js
-// Size = "4.0 Boys = 5.5 Women"
+// Size = "4.5 Boys = 6.0 Women"
+// https://www.vans.com/webapp/wcs/stores/servlet/VFAjaxGetMiniCartOrderView?storeId=10153&langId=-1&catalogId=10703&requesttype=ajax
